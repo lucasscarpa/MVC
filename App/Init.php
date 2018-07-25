f6353b3c820b23950237c0757eada2eb449d9ab8
Lucas Abrunhoza Scarpa <lucasabrunhoza@gmail.com>
2018-07-25 12:05:34 -0300

    ...

--- App/Init.php
+++ App/Init.php
@@ -5,10 +5,54 @@ namespace App;
 class Init
 {
 
+    private $routes;
+
     public function __construct()
     {
-        echo 'Carregou';
+        $this->InitRoutes();
+        $this->run($this->getUrl());
     }
-    
+
+    public function InitRoutes()
+    {
+        $ar['home'] = array(
+            'route' => '/',
+            'controller' => 'index',
+            'action' => 'index'
+        );
+
+        $ar['empresa'] = array(
+            'route' => '/empresa',
+            'controller' => 'index',
+            'action' => 'empresa'
+        );
+
+        $this->setRoutes($ar);
+
+    }
+
+    public function run($url)
+    {
+        array_walk($this->routes, function ($route) use ($url) {
+
+            if($url == $route['route']) {
+                $class = "App\\Controllers\\" . ucfirst($route['controller']);
+                $controller = new $class;
+                $action = $route['action'];
+                $controller->$action();
+            }
+        });
+    }
+
+    public function setRoutes(array $routes)
+    {
+        $this->routes = $routes;
+    }
+
+    public function getUrl()
+    {
+        return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    }
+
 }
 
